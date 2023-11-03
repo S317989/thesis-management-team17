ba2e2308fecdde3cfcf544d6798d421bd702fdbd
Andrea Alessandro Gaudino <andreasmacbookpro@MacBook-Pro-di-Andrea.local>
2023-11-03 12:58:28 +0100

    Project creation with vite and vitest config

---
+++ thesis-management/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>New App</p>
+    </>
+  )
+}
+
+export default App
---
+++ thesis-management/src/__tests__/App.test.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { describe, it, expect } from 'vitest'
+
+describe('App', () => {
+    it('renders a paragraph', () => {
+        expect(true).toBe(true);
+    })
+})
