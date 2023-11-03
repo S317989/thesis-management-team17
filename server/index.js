774a853546b570a8692ab7164540c8bff4775f23
Andrea Alessandro Gaudino <andreasmacbookpro@MacBook-Pro-di-Andrea.local>
2023-11-03 13:13:33 +0100

    Some library added

---
+++ server/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+// Init express
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(session({
+    secret: 'S3cr3tV4lu5_s3ss!0n',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api", require("./Router/RouterAPI"));
+
+
+// activate the server
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
