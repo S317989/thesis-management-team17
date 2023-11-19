20a7873b2ece028d6b640837701038bdbdf24934
Gizem Irmak <s314140@studenti.polito.it>
2023-11-19 21:19:26 +0100

    application decisions front end modified

--- client/src/Pages/ApplicationDecisions.jsx
+++ client/src/Pages/ApplicationDecisions.jsx
@@ -1,114 +1,127 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Table, Button } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../Contexts";
+import { Button, Container, Modal, Row, Col, Table } from "react-bootstrap";
+import sweetalert from "sweetalert";
+import ApplicationAPI from "../APIs/ApplicationAPI";
 
-const ApplicationDecisions = () => {
-  // Sample data, replace with actual data fetched from your API
-  const acceptedApplicationsData = [
-    {
-      id: 1,
-      title: 'Accepted Proposal 1',
-      supervisor: 'Dr. Smith',
-    },
-    // Add more accepted application data as needed
-  ];
+function ApplicationDecisions() {
+    
+    const { user } = useContext(UserContext);
 
-  const pendingApplicationsData = [
-    {
-      id: '',
-      title: '',
-      supervisor: '',
-    },
-    // Add more pending application data as needed
-  ];
-
-  const rejectedApplicationsData = [
-    {
-      id: 3,
-      title: 'Rejected Proposal 1',
-      supervisor: 'Dr. Brown',
-    },
-    // Add more rejected application data as needed
-  ];
-
-  // State to store applications
-  const [acceptedApplications, setAcceptedApplications] = useState([]);
-  const [pendingApplications, setPendingApplications] = useState([]);
-  const [rejectedApplications, setRejectedApplications] = useState([]);
-
-  useEffect(() => {
-    // Simulate fetching data from your API
-    setAcceptedApplications(acceptedApplicationsData);
-    setPendingApplications(pendingApplicationsData);
-    setRejectedApplications(rejectedApplicationsData);
-  }, []);
-
-  return (
-    <Container className="mt-4">
-      <Row>
-        <Col>
-          <h2 className="mb-4">Accepted Applications</h2>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Supervisor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {acceptedApplications.map((application) => (
-                <tr key={application.id}>
-                  <td>{application.title}</td>
-                  <td>{application.supervisor}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2 className="mb-4">Pending Applications</h2>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Supervisor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {pendingApplications.map((application) => (
-                <tr key={application.id}>
-                  <td>{application.title}</td>
-                  <td>{application.supervisor}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2 className="mb-4">Rejected Applications</h2>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Supervisor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {rejectedApplications.map((application) => (
-                <tr key={application.id}>
-                  <td>{application.title}</td>
-                  <td>{application.supervisor}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
-
-export default ApplicationDecisions;
+    const [myApplications, setMyApplications] = useState(null);
+    const [selectedApplication, setSelectedApplication] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+  
+    const renderApplications = () => {
+      ApplicationAPI.getMyApplications(user.id)
+        .then(async (response) => {
+          if (response.status === 200) {
+            const data = await response.json();
+            setMyApplications(data.applications);
+          }
+        });
+    };
+  
+    const handleViewDetails = (application) => {
+      setSelectedApplication(application);
+      setShowModal(true);
+    };
+  
+    useEffect(() => {
+      const checkAuthentication = async () => {
+        if (!user || user.role !== 'Student') {
+          sweetalert({
+            title: "You are not authorized to access this page",
+            icon: "error",
+            button: "Ok",
+          }).then(() => {
+            window.location.href = "http://localhost:3000/login";
+          });
+        } else {
+          renderApplications();
+        }
+      };
+  
+      checkAuthentication();
+    }, [user]);
+  
+    return (
+      <>
+        {user && user.role === 'Student' ? (
+          <>
+            <h1>My Applications</h1>
+  
+            {myApplications ? (
+              <>
+                <Container>
+                  <Row>
+                    <Col>
+                      <h2>Accepted Applications</h2>
+                      <Table striped bordered hover>
+                        <thead>
+                          <tr>
+                            <th>Title</th>
+                            <th>Supervisor</th>
+                            <th>Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {myApplications
+                            .filter((application) => application.status === 'Accepted')
+                            .map((application) => (
+                              <tr key={application.proposal_id}>
+                                <td>{application.proposal_title}</td>
+                                <td>{application.proposal_supervisor}</td>
+                                <td>
+                                  <Button variant="primary" onClick={() => handleViewDetails(application)}>
+                                    View Details
+                                  </Button>
+                                </td>
+                              </tr>
+                            ))}
+                        </tbody>
+                      </Table>
+                    </Col>
+                  </Row>
+                  {/* Similar tables for Pending and Rejected */}
+                </Container>
+  
+                {/* Details Modal */}
+                <Modal show={showModal} onHide={() => setShowModal(false)}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Application Details</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    {selectedApplication && (
+                      <>
+                        <p>Proposal ID: {selectedApplication.proposal_id}</p>
+                        <p>Title: {selectedApplication.proposal_title}</p>
+                        <p>Supervisor: {selectedApplication.proposal_supervisor}</p>
+                        <p>Keywords: {selectedApplication.proposal_keywords}</p>
+                        <p>Type: {selectedApplication.proposal_type}</p>
+                        <p>Description: {selectedApplication.proposal_description}</p>
+                        <p>Required Knowledge: {selectedApplication.proposal_requiredKnowledge}</p>
+                        <p>Notes: {selectedApplication.proposal_notes}</p>
+                        <p>Expiration: {selectedApplication.proposal_expiration}</p>
+                        <p>Level: {selectedApplication.proposal_level}</p>
+                      </>
+                    )}
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowModal(false)}>
+                      Close
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </>
+            ) : null}
+          </>
+        ) : (
+          <h1>Please, log in to visualize your information</h1>
+        )}
+      </>
+    );
+  }
+  
+  export default ApplicationDecisions;
+  
