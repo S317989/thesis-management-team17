7c4e7547b05b17241571e5397f369d5ad152c6fc
Andrea Alessandro Gaudino <andreasmacbookpro@MacBook-Pro-di-Andrea.local>
2023-11-28 12:38:46 +0100

    ActionButton refactor

--- client/src/Components/ActionButtons.jsx
+++ client/src/Components/ActionButtons.jsx
@@ -3,7 +3,7 @@ import { PencilFill, PlusSquareFill } from "react-bootstrap-icons";
 import { ArchiveFill } from "react-bootstrap-icons";
 import { Trash3Fill } from "react-bootstrap-icons";
 import { InfoSquareFill } from "react-bootstrap-icons";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { OverlayTrigger, Tooltip, Button } from "react-bootstrap";
 
 function ActionButtons(props) {
     const handleClick = () => {
@@ -14,20 +14,31 @@ function ActionButtons(props) {
     const actionComponents = {
         Delete: <Trash3Fill style={{ cursor: 'pointer', fontSize: '20px', marginRight: '20px', color: 'maroon' }} />,
         Archive: <ArchiveFill style={{ cursor: 'pointer', fontSize: '20px', marginRight: '20px', color: 'orange' }} />,
-        Add: <PlusSquareFill style={{ cursor: 'pointer', fontSize: '40px', color: '#007bff' }} />,
+        Add: (
+            <Button className="float-end" style={{ display: 'flex', alignItems: 'center' }}>
+                <PlusSquareFill style={{ cursor: 'pointer', fontSize: '30px' }} />
+                <span style={{ marginLeft: '5px' }}>New Proposal</span>
+            </Button>
+        ),
         Info: <InfoSquareFill style={{ cursor: 'pointer', fontSize: '20px', marginRight: '20px', color: 'purple' }} />,
         Update: <PencilFill style={{ cursor: 'pointer', fontSize: '20px', marginRight: '20px', color: 'green' }} />
     };
 
-    const overlayComponent = <Tooltip>{props.action}</Tooltip>
+    const overlayComponent = (
+        <Tooltip>
+            {props.action}
+        </Tooltip>
+    )
 
     return (
-        <OverlayTrigger
-            placement='top'
-            overlay={overlayComponent}>
-            {React.cloneElement(actionComponents[props.action], { onClick: handleClick })}
-        </OverlayTrigger >
+        props.action !== 'Add'
+            ?
+            <OverlayTrigger
+                placement='top'
+                overlay={overlayComponent}>
+                {React.cloneElement(actionComponents[props.action], { onClick: handleClick })}
+            </OverlayTrigger >
+            : React.cloneElement(actionComponents[props.action], { onClick: handleClick })
     )
 }
-
 export default ActionButtons;
