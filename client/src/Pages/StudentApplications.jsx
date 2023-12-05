5cb45c3fbae4d1c93541e77b4a859df3c3879062
Andrea Gaudino <gaudino00.ag@gmail.com>
2023-12-05 20:49:40 +0100

    Active/Pending application in progress

--- client/src/Pages/StudentApplications.jsx
+++ client/src/Pages/StudentApplications.jsx
@@ -1,5 +1,6 @@
+import "../Stylesheets/StudentApplicationStyle.css";
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Table, Card } from 'react-bootstrap';
+import { Container, Row, Col, Table, Card, Accordion, Badge } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from "../Contexts";
 import ProposalsAPI from "../APIs/ProposalsAPI";
@@ -13,6 +14,7 @@ import ApplicationsTable from '../Components/ApplicationsTable';
 import AuthenticationAPI from '../APIs/AuthenticationAPI';
 import { Pages } from '../APIs/AuthenticationAPI';
 import CardManager from '../Components/CardManager';
+import CustomCard from "../Components/CustomCard";
 
 const StudentApplications = () => {
     const navigate = useNavigate();
@@ -20,10 +22,12 @@ const StudentApplications = () => {
     const [availableProposals, setAvailableProposals] = useState([]);
     const [pendingOrActiveApplication, setPendingOrActiveApplication] = useState(null)
     const { user } = React.useContext(UserContext);
+    const [applicationStatus, setApplicationStatus] = useState("");
 
     async function fetchData() {
         const applicationsData = await ApplicationsAPI.getMyApplications();
         setApplications(applicationsData);
+
         setPendingOrActiveApplication(
             applicationsData.find((p) =>
                 p[ApplicationFields.Status] === ApplicationStatus.Pending ||
@@ -32,7 +36,17 @@ const StudentApplications = () => {
         setAvailableProposals((await ProposalsAPI.getAvailableProposalsForStudent()).filter(p =>
             !applicationsData.some(a => a[ApplicationFields.Proposal_Id] === p[ProposalFields.Id])
         ));
+
+
     }
+
+    useEffect(() => {
+        if (pendingOrActiveApplication)
+            pendingOrActiveApplication[ApplicationFields.Status] === ApplicationStatus.Pending
+                ? setApplicationStatus("Pending")
+                : setApplicationStatus("Active");
+
+    }, [pendingOrActiveApplication]);
 
     useEffect(() => {
         AuthenticationAPI.checkAuthenticationAPI(user.role, Pages.STUDENT_APPLICATIONS)
@@ -48,30 +62,68 @@ const StudentApplications = () => {
 
     return (
         <Container className="mt-4">
-            {pendingOrActiveApplication ? <PendingApplicationAlert application={pendingOrActiveApplication} /> : <></>}
-            <h3>Available Thesis Proposals</h3>
-            <CardManager page={"AvailableProposals"} proposals={availableProposals} EnableApplying={!pendingOrActiveApplication} requestRefresh={fetchData} />
-            <h3>Your Applications</h3>
-            <ApplicationsTable applications={applications} />
+            {
+                pendingOrActiveApplication ?
+                    (
+                        <>
+                            <Row className="mt-4 mb-4">
+                                <PendingApplicationAlert application={pendingOrActiveApplication} applicationStatus={applicationStatus} />
+                            </Row>
+                            <Row className="mt-4 mb-4">
+                                <Accordion>
+                                    <Accordion.Item eventKey="0">
+                                        <Accordion.Header className="text-center"><h4>Available Proposals</h4> </Accordion.Header>
+                                        <Accordion.Body>
+                                            <CardManager page={"AvailableProposals"} proposals={availableProposals} EnableApplying={!pendingOrActiveApplication} requestRefresh={fetchData} />
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                </Accordion>
+                            </Row>
+                        </>
+                    )
+                    : (
+                        <>
+                            <Row className="mt-4 mb-4">
+                                <h3>Available Proposals</h3>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <CardManager page={"AvailableProposals"} proposals={availableProposals} EnableApplying={!pendingOrActiveApplication} requestRefresh={fetchData} />
+                                </Col>
+                            </Row>
+                        </>
+                    )
+            }
+            <Row className="mt-4 mb-4">
+                <Accordion>
+                    <Accordion.Item eventKey="0">
+                        <Col>
+                            <Accordion.Header className="text-center"><h4>Old Applications</h4> </Accordion.Header>
+                            <Accordion.Body>
+                                <ApplicationsTable applications={applications} />
+                            </Accordion.Body>
+                        </Col>
+                    </Accordion.Item>
+                </Accordion>
+            </Row>
         </Container>
     );
 };
 
-const PendingApplicationAlert = ({ application }) => {
+const PendingApplicationAlert = ({ application, applicationStatus }) => {
     const proposal = application[ApplicationFields.Proposal];
-    return <Card>
-        <Card.Header>You have {
-            application[ApplicationFields.Status] === ApplicationStatus.Pending ?
-                'a pending' : 'an active'
-        } application for</Card.Header>
-        <Card.Body>
-            <Card.Title>{proposal[ProposalFields.Title]}</Card.Title>
-            <Card.Text>
-                This thesis proposal is supervised by {proposal.Supervisor.Name + ' ' + proposal.Supervisor.Surname}
-            </Card.Text>
-            <ShowProposalsForm proposal={proposal} />
-        </Card.Body>
-    </Card>
+    return (
+        <Container className={`parent-pending-card-container-${applicationStatus}`}>
+            <div className="child-pending-card-container">
+                <h4>{applicationStatus} Application</h4>
+                <CustomCard
+                    id="student-application-card"
+                    proposal={proposal}
+                    OnComplete={() => { }}
+                />
+            </div>
+        </Container>
+    );
 }
 
 export default StudentApplications;
