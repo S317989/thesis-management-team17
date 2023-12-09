a440cd0311d94824019cba52e4e0b713c3f4612e
Gizem Irmak <gizemirmak97@gmail.com>
2023-12-09 21:18:18 +0100

    acess applicant cv frontend is done

--- client/src/Components/ApplicationsActions.jsx
+++ client/src/Components/ApplicationsActions.jsx
@@ -1,43 +1,83 @@
-import { Button } from 'react-bootstrap';
-import ApplicationsAPI from "../APIs/ApplicationsAPI";
-import sweetalert from "sweetalert";
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import ApplicationsAPI from '../APIs/ApplicationsAPI';
+import sweetalert from 'sweetalert';
 
 export const Apply = ({ proposalId, OnComplete }) => {
-    const handleDelete = () => {
-        sweetalert({
-            title: "Are you sure you want to apply to this proposal?",
-            text: "Once you apply you will not be able to make other applications untill this one is accepted or rejected",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        }).then(confirmed => {
-            if (confirmed) {
-                ApplicationsAPI.applyToProposal(proposalId).then((result) => {
-                    if (result.status === 200) {
-                        sweetalert({
-                            title: "Application Made",
-                            icon: "success",
-                            button: "Ok",
-                        }).then(() => { if (OnComplete) OnComplete() });
+  const [showModal, setShowModal] = useState(false);
+  const [cvFile, setCvFile] = useState(null);
 
-                    }
-                    else {
-                        sweetalert({
-                            title: "Application couldn't be made.",
-                            icon: "error",
-                            button: "Ok",
-                        });
-                    }
-                })
-            }
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setCvFile(file);
+  };
+
+  const handleApply = () => {
+    handleCloseModal(); // Close the modal before making the application request
+
+    sweetalert({
+      title: 'Are you sure you want to apply to this proposal?',
+      text: 'Once you apply, you will not be able to make other applications until this one is accepted or rejected',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    }).then((confirmed) => {
+      if (confirmed) {
+        ApplicationsAPI.applyToProposal(proposalId, cvFile).then((result) => {
+          if (result.status === 200) {
+            sweetalert({
+              title: 'Application Made',
+              icon: 'success',
+              button: 'Ok',
+            }).then(() => {
+              if (OnComplete) OnComplete();
+            });
+          } else {
+            sweetalert({
+              title: "Application couldn't be made.",
+              icon: 'error',
+              button: 'Ok',
+            });
+          }
         });
-    };
-    return <>
-        <Button style={{ backgroundColor: '#23527c', border: 'none' }} onClick={() => handleDelete()}>
+      }
+    });
+  };
+
+  return (
+    <>
+      <Button style={{ backgroundColor: '#23527c', border: 'none' }} onClick={handleShowModal}>
+        Apply
+      </Button>
+
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Upload CV</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input type="file" accept=".pdf, .doc, .docx" onChange={handleFileChange} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleApply}>
             Apply
-        </Button>{' '}
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
+  );
 };
+
 
 export const Accept = ({ applicationId, OnComplete }) => {
     const handleDelete = () => {
