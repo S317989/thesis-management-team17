b99d35aacaded45e124799d43dbc03a5572457b2
Andrea Gaudino <gaudino00.ag@gmail.com>
2023-12-13 15:34:15 +0100

    Multer fix in progress

--- server/Services/pdfUploadMiddleware.js
+++ server/Services/pdfUploadMiddleware.js
@@ -1,5 +1,7 @@
 const multer = require('multer');
 
+multer({ limits: { fileSize: 8000000 } });
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -8,21 +10,10 @@ const storage = multer.diskStorage({
         const timestamp = Date.now();
         const newFileName = `${timestamp}_${file.originalname}`;
         cb(null, newFileName);
-    },
+    }
 });
 
-const upload = multer({ storage: storage, limits: { fileSize: 8000000 } }, fileFilter = (req, file, cb) => {
-    if (file.size > 8000000) {
-        return cb(new Error('Il file supera la dimensione massima consentita.'));
-    }
-
-    if (!file.mimetype.startsWith('application/pdf')) {
-        return cb(new Error('File must be PDF.'));
-    }
-
-    cb(null, true);
-
-});
+const upload = multer({ storage: storage, limits: { fileSize: 8000000 } });
 
 const handleFileUpload = upload.single('pdfFile');
 module.exports = handleFileUpload;
