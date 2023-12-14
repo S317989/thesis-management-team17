9edf51b9373375d75cc5d000796d857a9f25f0df
FedericoSanfra <s308344@studenti.polito.it>
2023-12-14 14:28:43 +0100

    changed script and refacotred applciationproposal_e2e, moved in jest

--- server/jest/e2e/ApplicationProposal.e2e.js
+++ server/jest/e2e/ApplicationProposal.e2e.js
@@ -60,19 +60,26 @@ describe("End to end tests for list of applications and check for notifications"
   };
 
   async function doLogout() {
+    await driver.get(baseURL);
+    await driver.sleep(1000);
+
     await checkAndCollapseNavbar();
+  
+    await driver.sleep(1000);
 
     const logoutLinkButton = await driver.findElement(By.css('#link-logout-navbar-button'));
     await logoutLinkButton.click();
+
+    await driver.sleep(1000);
   }
  //try to use wait instead of sleep for better synchro
-  beforeAll(async () => {
-    driver = await new Builder().forBrowser("chrome").build();
-  });
+ beforeAll(async () => {
+  driver = await new Builder().forBrowser("chrome").build();
+});
 
-  afterAll(async () => {
-    await driver.quit();
-  });
+afterAll(async () => {
+  await driver.quit();
+});
 
   test("Should show list of applications after clicking on Applications tab in the navbar and see info applications opening a modal", async () => {
     await doLogin();
@@ -155,78 +162,6 @@ describe("End to end tests for list of applications and check for notifications"
     
   }, 20000);
 
-});
-
-describe("End to end tests for list of All Proposals", () => {
-
-  async function isElementVisible(selector) {
-    try {
-      const element = await driver.findElement(By.css(selector));
-      return await element.isDisplayed();
-    } catch (error) {
-      // If the element is not found, consider it not visible
-      return false;
-    }
-  }
-  
-  let driver;
-  let baseURL = `http://localhost:5173`;
-
-  async function checkAndCollapseNavbar() {
-    const isNavbarCollapsed = await isElementVisible('.navbar-toggler-icon');
-    if (isNavbarCollapsed) {
-      const hamburgerMenuIcon = await driver.findElement(By.css('.navbar-toggler-icon'));
-      await hamburgerMenuIcon.click();
-    }
-  }
-
-  const doLogin = async () => {
-    await driver.get(baseURL);
-
-    await driver.sleep(1000);
-
-    // perform login
-    let usernameBox = await driver.findElement(By.id("username"));
-    usernameBox.clear();
-    usernameBox.sendKeys("d12571@polito.it");
-    let passwordBox = await driver.findElement(By.id("password"));
-    passwordBox.clear();
-    passwordBox.sendKeys("d12571");
-
-    await driver.sleep(1000);
-
-    const submitButton = await driver.findElement(By.css("button.c1939bbc3.cc78b8bf3.ce1155df5.c1d2ca6e3.c331afe93"));
-
-    // remove disabled property from button
-    await driver.executeScript(
-      "arguments[0].removeAttribute('disabled')",
-      submitButton
-    );
-
-    // click submit button with js
-    await submitButton.click();
-
-    await driver.sleep(1000);
-
-    await driver.get(baseURL);
-
-    await driver.sleep(1000);
-  };
-
-  async function doLogout() {
-    await checkAndCollapseNavbar();
-
-    const logoutLinkButton = await driver.findElement(By.css('#link-logout-navbar-button'));
-    await logoutLinkButton.click();
-  }
- //try to use wait instead of sleep for better synchro
-  beforeAll(async () => {
-    driver = await new Builder().forBrowser("chrome").build();
-  });
-
-  afterAll(async () => {
-    await driver.quit();
-  });
 
   test("Should show list of thesis proposals, open cards, filter and close", async () => {
     await doLogin();
@@ -292,79 +227,6 @@ describe("End to end tests for list of All Proposals", () => {
       await doLogout();
     
   }, 20000);
-
-});
-
-describe("End to end tests for active proposal and create a New Proposal succesfully", () => {
-
-  async function isElementVisible(selector) {
-    try {
-      const element = await driver.findElement(By.css(selector));
-      return await element.isDisplayed();
-    } catch (error) {
-      // If the element is not found, consider it not visible
-      return false;
-    }
-  }
-  
-  let driver;
-  let baseURL = `http://localhost:5173`;
-
-  async function checkAndCollapseNavbar() {
-    const isNavbarCollapsed = await isElementVisible('.navbar-toggler-icon');
-    if (isNavbarCollapsed) {
-      const hamburgerMenuIcon = await driver.findElement(By.css('button[aria-controls="responsive-navbar-nav"].navbar-toggler'));
-      await hamburgerMenuIcon.click();
-    }
-  }
-
-  const doLogin = async () => {
-    await driver.get(baseURL);
-
-    await driver.sleep(1000);
-
-    // perform login
-    let usernameBox = await driver.findElement(By.id("username"));
-    usernameBox.clear();
-    usernameBox.sendKeys("d12571@polito.it");
-    let passwordBox = await driver.findElement(By.id("password"));
-    passwordBox.clear();
-    passwordBox.sendKeys("d12571");
-
-    await driver.sleep(1000);
-
-    const submitButton = await driver.findElement(By.css("button.c1939bbc3.cc78b8bf3.ce1155df5.c1d2ca6e3.c331afe93"));
-
-    // remove disabled property from button
-    await driver.executeScript(
-      "arguments[0].removeAttribute('disabled')",
-      submitButton
-    );
-
-    // click submit button with js
-    await submitButton.click();
-
-    await driver.sleep(1000);
-
-    await driver.get(baseURL);
-
-    await driver.sleep(1000);
-  };
-
-  async function doLogout() {
-    await checkAndCollapseNavbar();
-
-    const logoutLinkButton = await driver.findElement(By.css('#link-logout-navbar-button'));
-    await logoutLinkButton.click();
-  }
- //try to use wait instead of sleep for better synchro
-  beforeAll(async () => {
-    driver = await new Builder().forBrowser("chrome").build();
-  });
-
-  afterAll(async () => {
-    await driver.quit();
-  });
 
   test("create a new Proposal", async () => {
     await doLogin();
